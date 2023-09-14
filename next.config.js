f8029f1e0bce85c55eb9f5d50468a753c1734e85
Liaksej <tarusov.liaksej@gmail.com>
2023-09-14 16:11:19 +0200

    Configure asset path for production in next.config
    The assetPrefix in next.config.js has been updated to include the production path for the 'ra-components-store-func' repository. This change ensures the accurate referencing of assets in a production environment. Additionally, the global styles import path was adjusted in layout.tsx to maintain congruity in file referencing.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: "export",
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "/ra-components-store-func" : "",
+    process.env.NODE_ENV === "production"
+      ? "https://liaksej.github.io/ra-components-store-func/"
+      : undefined,
 };
 
 module.exports = nextConfig;
